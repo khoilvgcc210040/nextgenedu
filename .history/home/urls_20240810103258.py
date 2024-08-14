2c423c73fba7d4af7a60da077f48d5a222098a82
khoilvgcc210040 <khoilieuct03@gmail.com>
2024-08-14 15:38:40 +0700

    delete history file

--- .history/home/urls_20240810103258.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('', views.auth, name='auth'),
-    path('', views.home, name='home'),
-]
--- .history/home/urls_20240810184217.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('auth/', views.authPage, name='auth'),
-    path('logout/', views.logoutPage, name='logout'),
-    path('subjects/', views.subjects, name='subjects'),
-]
